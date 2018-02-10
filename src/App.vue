8afcbe350b3567ab30a8f900dd6131dc5bbd23fa
Nikhil Warke <nikwarke@gmail.com>
2018-02-10 17:42:12 +0530

    Prepare project
    Remove generated content from App.vue.
    Keep it minimalistic
    
    Add material icons,  auto trim whitespace and semi false
    Remove auto generated Hello.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
-    <header></header>
-    <main>
-      <img src="~assets/quasar-logo-full.svg" alt="Quasar PWA">
       <router-view></router-view>
-    </main>
   </div>
 </template>
 
@@ -16,15 +12,5 @@
 export default {}
 </script>
 
-<style lang="stylus">
-@import '~variables'
-
-main
-  text-align center
-  margin-top 40px
-
-header
-  margin 0
-  height 10px
-  background-color $primary
+<style>
 </style>
